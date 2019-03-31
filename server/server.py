73f8217be21910bd0087002b0ea663b0fb80f21a
guizo <guizoxxv@gmail.com>
2019-03-31 10:39:45 -0300

    Connecting to database

--- server/server.py
+++ server/server.py
@@ -1,6 +1,23 @@
 from flask import Flask, request, jsonify
+# from pymongo import MongoClient
+from flask_mongoalchemy import MongoAlchemy
 
 app = Flask(__name__)
+
+# client = MongoClient('mongodb://root:root@172.17.0.1:3012/')
+
+# db = client.movies_api
+
+app.config['MONGOALCHEMY_DATABASE'] = 'movies_api'
+app.config['MONGOALCHEMY_SERVER'] = '172.17.0.1'
+app.config['MONGOALCHEMY_PORT'] = '3012'
+app.config['MONGOALCHEMY_USER'] = 'root'
+app.config['MONGOALCHEMY_PASSWORD'] = 'root'
+
+db = MongoAlchemy(app)
+
+class Example(db.Document):
+    name = db.StringField()
 
 @app.route('/', methods=['GET'])
 def home():
