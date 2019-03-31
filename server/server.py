14241bbefd6ee3db3e202c37e54a89ce5bb8bfce
guizo <guizoxxv@gmail.com>
2019-03-31 09:22:37 -0300

    Dockerized environment - Python Flask + MongoDB

---
+++ server/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({ 'message': 'Hello World' })
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
